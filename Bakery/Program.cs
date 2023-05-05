3f81d1e933b98f710feeaa2db487ff85ca33036b
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-05 15:51:21 -0700

    add user input methods and branching.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Bakery.Models;
 
 namespace Bakery
@@ -28,8 +29,25 @@ namespace Bakery
       Console.WriteLine("So, what'll it be, stranger?");
       LoafEnter();
       int total = Bread.AllBreadPrice();
-      Console.WriteLine($"A total of {total}");
-
+      Console.WriteLine($"A total of {total}.");
+      BreadSaleMessage();
+      Console.WriteLine("How about some pastries? (y/n)");
+      string proceed = Console.ReadLine();
+      if (proceed.ToLower() == "y")
+      {
+        PastryEnter();
+        int totalP = Pastry.AllPastryPrice();
+        Console.WriteLine($"A total of {totalP}.");
+      }
+      else
+      {
+        Console.WriteLine($"Alrighty, Your grand total for today is {total}!");
+        Console.WriteLine("Have a nice day, now!");
+      }
+      Console.WriteLine("Your grand total for today is...");
+      int grandTotal = total + Pastry.AllPastryPrice();
+      Console.WriteLine($"... {grandTotal}! What a haul!");
+      
     }
 
     static void LoafEnter()
@@ -47,11 +65,48 @@ namespace Bakery
         LoafEnter();
       }
     }
+
     static void MakeLoaves(int breadNumber)
     {
       for (int i = 0; i <= breadNumber; i++) 
       {
-      Bread newBread = new Bread(5, i); 
+        Bread newBread = new Bread(5, i); 
+      }
+    }
+
+    static string BreadSaleMessage()
+    {
+      if (Bread.AllBreadPrice() % 10 == 0)
+      {
+        return "That's $5 off every third loaf! Thank you!";
+      }
+      else
+      {
+        return "Thank you.";
+      }
+    }
+
+    static void MakePastries(int pastryNumber)
+    {
+      for (int i = 0; i <= pastryNumber; i++)
+      {
+        Pastry newPastry = new Pastry(2, i);
+      }
+    }
+
+    static void PastryEnter()
+    {
+      try
+      {
+        Console.WriteLine("How many pastries would you like? (enter a number)");
+        int pastryNumber = Convert.ToInt32(Console.ReadLine());
+        MakePastries(pastryNumber);
+        Console.WriteLine($"{pastryNumber} pastries, right? that'll be...");
+      }
+  catch
+      {
+        Console.WriteLine("I'm not sure what number that is. Run that by me again?");
+        PastryEnter();
       }
     }
   }
