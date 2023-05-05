5101fdc84373afa015cfd632e160dca5689f44c1
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-05 14:49:50 -0700

    add intro text to Program.cs.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,20 @@ namespace Bakery
   {
     static void Main()
     {
-      
+    RunBakery();
+    }
+    static void RunBakery()
+    {
+      Console.WriteLine("~~~~~~~~~....~~~~~~~~~....~~~~~~~~~....~~~~~~~~");
+      Console.WriteLine("Welcome to Pierre's Bakery");
+      string today = DateTime.Now.ToString("ddd");
+      Console.WriteLine($"Today is {today}");
+      Console.WriteLine("Great weather we're having!");
+      Console.WriteLine($"Our {today} specials are:");
+      Console.WriteLine("Bread: Buy 2 Loaves, get 1 Free!");
+      Console.WriteLine("and...");
+      Console.WriteLine("Pastry: Buy 3 Pastries, get 1 Free!");
+      Console.WriteLine("~~~~~~~~~....~~~~~~~~~....~~~~~~~~~....~~~~~~~~");
     }
   }
 }
