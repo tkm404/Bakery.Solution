40e686bba3f6e42c11a3abe5994edccf9b68c154
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-05 08:44:37 -0700

    add boilerplate for classes and class tests.

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    
+  }
+}
---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,7 @@
+namespace Bakery.Models
+{
+  public class Pastry
+  {
+    
+  }
+}
