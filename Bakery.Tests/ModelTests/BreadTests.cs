0a2bd24d91600713df685e370449baea4475476d
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-05 08:53:37 -0700

    add test preparation for Bread class, add Main method to Program.

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -6,6 +6,12 @@ namespace Bakery.Tests
   [TestClass]
   public class BreadTests
   {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread(1, 5);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
     
   }
 }
---
+++ Bakery/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      
+    }
+  }
+}
