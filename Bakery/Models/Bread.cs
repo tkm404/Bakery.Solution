1a2eb1ed6005472d54106b348e93d381fa0468aa
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-05 09:27:58 -0700

    pass test for Bread constructor. Updated Bread Model with price and unit number.

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Collections.Generic;
+
 namespace Bakery.Models
 {
   public class Bread
   {
-    //Bread has price and number of units
+    //Bread has price and number of units, with every third unit free.
+    public int price { get; set; }
+    public int loaf { get; set; }
+    private static List<Bread> _breadCart = new List<Bread> {};
+
+    public Bread(int priceOf, int loafNumber)
+    {
+      price = priceOf;
+      loaf = loafNumber;
+      _breadCart.Add(this);
+    }
+
   }
 }
