66d6e4c455c43d58902e20849d0f65bfee8fe745
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-05 09:26:35 -0700

    update 'using' call to make sure everything in Models can be called.

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
+using System.Collections.Generic;
+using System;
 
 namespace Bakery.Tests
 {
@@ -9,7 +11,7 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(5, 1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
